11671b69c36aae5bd204ec52c5a4ef38506d33c6
Tej_Kapadiya <107534599+TejKapadiya@users.noreply.github.com>
2026-03-16 13:08:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template,request,redirect,url_for # For flask implementation
 from bson import ObjectId # For ObjectId to work
 from pymongo import MongoClient
+from dotenv import load_dotenv
 import os
 
 app = Flask(__name__)
